29df00235fd08cca991e8a6d8077ac207f4af221
muralitharan <muralitharan805@gmail.com>
2022-05-08 17:34:51 +0530

    err

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular_practice';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const observable = new Observable<number>((observer) => {
+      let x = 0;
+
+      const interval = setInterval(() => {
+        if (x === 5) {
+          observer.error('Oops');
+        } else {
+          observer.next(x++);
+        }
+      }, 1000);
+
+      return {
+        unsubscribe: () => clearInterval(interval),
+      };
+    });
+
+    observable.pipe(catchError((err) => of(-1))).subscribe({
+      next: (x) => console.log(`${x} received`),
+      complete: () => console.log('Completed'),
+      error: (err) => console.warn(err),
+    });
+  }
 }
