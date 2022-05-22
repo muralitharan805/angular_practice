21177be571f317ed72963895a00237fb4e5ae640
muralitharan <muralitharan805@gmail.com>
2022-05-22 18:03:46 +0530

    basic side and header design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { SharedModule } from './shared/shared.module';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
+    MatGridListModule,
     SharedModule,
   ],
   providers: [],
