df5edfdde61723e8fc13e970b722c283e95dfa8e
muralitharan <muralitharan805@gmail.com>
2022-06-05 12:45:03 +0530

    tree pratice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,55 @@ import {
   MatTreeFlattener,
 } from '@angular/material/tree';
 
+/** Flat node with expandable and level information */
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Fruit',
+    children: [{ name: 'Apple' }, { name: 'Banana' }, { name: 'Fruit loops' }],
+  },
+  {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [{ name: 'Broccoli' }, { name: 'Brussels sprouts' }],
+      },
+      {
+        name: 'Orange',
+        children: [{ name: 'Pumpkins' }, { name: 'Carrots' }],
+      },
+    ],
+  },
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  private _transformer = (node: FoodNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  };
+
   title = 'angular_practice';
 
   isMenuClick = true;
@@ -19,5 +62,19 @@ export class AppComponent {
   onCollapse() {
     this.isCollapse = !this.isCollapse;
   }
-  constructor() {}
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    (node: any) => node.level,
+    (node: any) => node.expandable,
+    (node: any) => node.children
+  );
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 }
