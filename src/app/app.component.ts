a62628e8d538d25ecc09ac8e128c6012cbd619da
murali <muralitharan805@gmail.com>
2022-07-09 18:16:09 +0530

    mat side nav tool bar responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular_practice';
+  IsmediaQureyMatch = false;
+  IsToggle = true;
+  constructor(public breakpointObserver: BreakpointObserver) {}
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe(['(max-width: 600px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.IsmediaQureyMatch = true;
+          console.log(
+            'Viewport width is 600px or greater!',
+            this.IsmediaQureyMatch
+          );
+        } else {
+          console.log(
+            'Viewport width is less than 600px',
+            this.IsmediaQureyMatch
+          );
+          this.IsmediaQureyMatch = false;
+        }
+      });
+  }
+
+  fillerContent = Array.from(
+    { length: 50 },
+    () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+  );
 }
